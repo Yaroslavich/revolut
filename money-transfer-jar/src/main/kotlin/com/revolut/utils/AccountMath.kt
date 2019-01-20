263294bc52ce673c8e60212311d1d19f35fa02d8
Yaroslav <yaroslavich@gmail.com>
2019-01-20 17:15:44 +0300

    account math: setScale

--- money-transfer-jar/src/main/kotlin/com/revolut/utils/AccountMath.kt
+++ money-transfer-jar/src/main/kotlin/com/revolut/utils/AccountMath.kt
@@ -10,28 +10,34 @@ import java.math.MathContext
  */
 
 object AccountMath {
-    private val mathContext: MathContext = MathContext(4)
+    private const val defaultScale = 4
+    private const val defaultDigits = 10
+    private val mathContext: MathContext = MathContext(defaultDigits + defaultScale)
 
-    val thousand = BigDecimal(1000, mathContext)
-    val hundred = BigDecimal(100, mathContext)
-    val one = BigDecimal(1, mathContext)
-    val zero = BigDecimal(0, mathContext)
+    val thousand = fromInt(1000)
+    val hundred = fromInt(100)
+    val one = fromInt(1)
+    val zero = fromInt(0)
 
-    val minusOne = BigDecimal(-1, mathContext)
-    val aLot = BigDecimal(1000000, mathContext)
+    val minusOne = fromInt(-1)
+    val aLot = fromInt(1000000)
+
+    private fun fromInt(amount: Int): BigDecimal {
+        return BigDecimal(amount, mathContext).setScale(defaultScale)
+    }
 
     fun withdraw(from: BigDecimal, amount: BigDecimal): BigDecimal {
-        return from.subtract(amount, mathContext)
+        return from.setScale(defaultScale).subtract(amount, mathContext)
     }
 
     fun deposit(from: BigDecimal, amount: BigDecimal): BigDecimal {
-        return from.add(amount, mathContext)
+        return from.setScale(defaultScale).add(amount, mathContext)
     }
 
     fun sum(amounts: List<BigDecimal>): BigDecimal {
         if (amounts.isEmpty()) {
             return zero
         }
-        return amounts.fold(zero, BigDecimal::add)
+        return amounts.fold(zero.setScale(defaultScale), BigDecimal::add)
     }
 }
